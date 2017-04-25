c3acf86c31ceb2418468f24851903038c544da1c
gideonw14 <gswb7d@mst.edu>
2017-04-25 02:08:40 -0500

    pls js

--- js/test.js
+++ js/test.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
      $("#donald").click(function(){
-     	alert("button clicked");
-    //     $.post("/users",			//URL
-    //     {							//Data
-    //       name: "Donald Duck",
-    //       password: "Password123",
-    //       email: "donald@duck.org"
-    //     },
-    //     function(data,status){ 		//Callback
-    //         alert("Data: " + data + "\nStatus: " + status);
-    //     });
+     	alert("donald button clicked");
+        $.post("/users",			//URL
+        {							//Data
+          name: "Donald Duck",
+          password: "Password123",
+          email: "donald@duck.org"
+        },
+        function(data,status){ 		//Callback
+            alert("Data: " + data + "\nStatus: " + status);
+        });
     });
 });
 
