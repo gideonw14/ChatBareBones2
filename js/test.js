6ac7f49632db1814fee1058e4051800d2e5a4a08
gideonw14 <gswb7d@mst.edu>
2017-04-24 23:48:47 -0500

    pls js

--- js/test.js
+++ js/test.js
@@ -18,6 +18,7 @@ $('form').submit(function() {
 });
 
 function validateForm() {
+	console.log("IM GOING CRAZY");
     var x = document.forms["myForm"]["fname"].value;
     if (x == "") {
         alert("Name must be filled out");
