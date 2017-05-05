95da0a061072c195fd99f222a3ab5cab7f531da0
gideonw14 <gswb7d@mst.edu>
2017-05-05 02:56:06 -0500

    ADDED CHAT FEATURE

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@
 //     $('#menu').load('menu.html');
 // });
 
+function loadUserInfo(){
+  console.log($.session.get("username"));
+  var element = document.getElementById("username");
+  element.innerHTML = "This here is " + $.session.get("name");
+}
+
 // Donald Duck Feature
 $(document).ready(function(){
      $("#donald").click(function(){
@@ -62,23 +68,26 @@ $(document).ready(function(){
 $(document).ready(function(){
   $("#login").submit(function(){
     if(!($.session.get("key") == "")){
-      var data = JSON.stringify($(this).serializeArray());
-      data = JSON.parse(data);
+      var login = JSON.stringify($(this).serializeArray());
+      login = JSON.parse(login);
       var user = {
-        username: data[0].value,
-        password: data[1].value
+        username: login[0].value,
+        password: login[1].value
       }
-      data = JSON.stringify(user);
-      console.log(data);
+      login = JSON.stringify(user);
+      console.log(login);
       $.ajax({
         type: "POST",
         url: "/api/session",
-        data: data,
+        data: login,
         success: function(data, status){
           alert(user.username + " successfully logged in");
-          $.session.set("key", data);
+          console.log(data["session-key"]);
+          $.session.set("key", data["session-key"]);
           $.session.set("name", user.username);
-          window.location.replace("/index");
+          console.log($.session.get("name") + "\n" + $.session.get("key"));
+          console.log("wait right there");
+          window.location.replace("/account");
         },
         statusCode:{
           422: function(){
@@ -97,18 +106,25 @@ $(document).ready(function(){
 // User Logout
 $(document).ready(function(){
   $("#logout").click(function(){
+    console.log($.session.get("key"));
     $.ajax({
       type: "DELETE",
       url: "/api/session",
-      data: $.session.get("key"),
+      headers: {
+          "session-key": $.session.get("key"),
+          "Content-Type": "application/json"
+      },
       success: function(data, status){
         alert("User logged out");
         $.session.remove("key");
+        $.session.remove("username");
+        window.location.replace("/index");
       },
-      // statusCode:{
-
-      // },
-      contentType: "application/json"
+      statusCode:{
+        410: function(){
+          alert("User could not be logged out - gone");
+        }        
+      },
     });
   });
 });
@@ -117,6 +133,76 @@ function hello() {
   alert("Hello! You submit the form");
   return false;
 }
+
+//Sending a PM
+$(document).ready(function(){
+  $("#submit-input").submit(function(){
+    var formData = JSON.stringify($(this).serializeArray());
+    formData = JSON.parse(formData);
+    var address = "/api/users/" + $.session.get("name") + 
+    "/pm/" + formData[0].value;
+    console.log(address);
+    var sendMessage = JSON.stringify({
+      message: formData[1].value
+    });
+    console.log(sendMessage);
+    $.ajax({
+      type: "POST",
+      url: address,
+      headers: {
+        "session-key": $.session.get("key"),
+        "Content-Type": "application/json"
+      },
+      data: sendMessage,
+      success: function(data, status){
+        console.log("message sent successfully");
+      },
+      statusCode:{
+        404: function(){
+          alert("No user named " + formData[0].value);
+        }
+      },
+      contentType: "application/json"
+    });
+    console.log("wait right there");
+  });
+});
+
+//Get messages
+$(document).ready(function(){
+  $("#refresh").submit(function(){
+    var formData = JSON.stringify($(this).serializeArray());
+    formData = JSON.parse(formData);
+    var address = "/api/users/" + $.session.get("name") + 
+    "/pm/" + formData[0].value;
+    console.log(address);
+    $.ajax({
+      type: "GET",
+      url: address,
+      headers: {
+        "session-key": $.session.get("key"),
+        "Content-Type": "application/json"
+      },
+      success: function(data){
+        console.log(data);
+        var messageString = "";
+        for(var i in data.messages){
+          messageString = messageString + "FROM: " +
+          data.messages[i].sender + " -> " + 
+          data.messages[i].msg + "\n";
+        }
+        console.log(messageString);
+        document.getElementById("general-chat").innerHTML = messageString;
+      },
+      statusCode:{
+        404: function(){
+          alert("No chat history with " + formData[0].value)
+        }
+      },
+      contentType: "application/json"
+    });
+  });
+});
 
 //=======================================
 // Derpy functions I stole from W3 schools
